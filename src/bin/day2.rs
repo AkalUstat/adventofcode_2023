4fabded5c55145916de6305702dd01de946e28a7
AkalUstat <akalustat.singh@gmail.com>
2023-12-02 11:46:59 -0800

    part two day two

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,13 +3,12 @@ use adventofcode_2023::file_reader;
 use std::io::{BufRead};
 use regex::Regex;
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> i32{
     let filer = file_reader("./inputs/day2.txt");
     let id_regex = Regex::new(r"(\d+)").unwrap();
-    let _values_regex = Regex::new(r"Game (\d+)").unwrap();
 
     let (max_red, max_green, max_blue) = (12i32, 13i32, 14i32);
     let mut id_collector = 0i32;
@@ -44,21 +43,56 @@ fn part_one() -> i32{
                     }
                 }
             }
-
         }
-
         id_collector += id.parse::<i32>().unwrap();
-
-
-
-
-
-
     }
     id_collector
-
 }
 
-fn part_two(){}
+fn part_two() -> i32 {
+    let filer = file_reader("./inputs/day2.txt");
+
+    let mut power_collector = 0i32;
+
+    'outer: for line in filer.lines().map(|l| l.unwrap()) {
+        let line_borrow = &line;
+        let mut separate_id = line_borrow.split(":");
+        let value_str = separate_id.last().unwrap();
+        let mut max_red_num = 0i32;
+        let mut max_green_num = 0i32;
+        let mut max_blue_num = 0i32;
+        for value_set in value_str.split(";") {
+            let set = &value_set;
+
+            for color_set in set.split(", ") {
+                let vec = color_set.split(" ").filter(|c| c != &"").collect::<Vec<&str>>();
+                match vec[1] {
+                    "red" => {
+                        let val = vec[0].parse::<i32>().unwrap();
+                        if val > max_red_num {
+                            max_red_num = val;
+                        }
+                    },
+                    "blue" => {
+                        let val = vec[0].parse::<i32>().unwrap();
+                        if val > max_blue_num {
+                            max_blue_num = val;
+                        }
+                    },
+                    "green" => {
+                        let val = vec[0].parse::<i32>().unwrap();
+                        if val > max_green_num {
+                            max_green_num = val;
+                        }
+                    },
+                    _ => {
+                    }
+                }
+            }
+        }
+        power_collector += (max_red_num * max_blue_num * max_green_num);
+    }
+    power_collector
+}
 
 
