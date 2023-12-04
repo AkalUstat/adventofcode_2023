6b37a895dd29fb579e8062f8898f3622612b545b
akalustat <akalustat.singh@gmail.com>
2023-12-04 14:27:56 -0800

    Add new function for getting Vec<lines>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 use std::fs::File;
-use std::io::{BufReader};
+use std::io::{BufReader, BufRead};
 
 
 pub fn file_reader(file_loc: &str) -> BufReader<File> {
     let file = File::open(file_loc).unwrap();
     let reader = BufReader::new(file);
     reader
+}
+
+pub fn get_files_lines(file_loc: &str) -> Vec<String> {
+    let file_r: BufReader<_> = file_reader(file_loc);
+    file_r.lines().map(|l| l.unwrap()).collect()
 }
 
