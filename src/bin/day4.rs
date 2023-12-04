7854f49e9e5d45c8141c85bf0441f286ccf3465d
AkalUstat <akalustat.singh@gmail.com>
2023-12-04 09:52:05 -0800

    (day 4): begin part two

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -7,11 +7,14 @@ fn main() {
     println!("{}", part_one());
 }
 
-fn part_one() -> usize {
+fn part_one() -> (usize, usize) {
     let file_r = file_reader("./inputs/day4.txt");
     // map into readable data
     let mut nums_collector = 0;
-    for (_indx, ln) in file_r.lines().map(|l| l.unwrap()).enumerate() {
+    let mut nested_nums_collector = 0;
+    let mut lines = HashMap::new();
+
+    for (indx, ln) in file_r.lines().map(|l| l.unwrap()).enumerate() {
         let line = &ln;
         let split: Vec<&str> = line.split(":").collect();
         let strings: Vec<&str> = split[1].split("|").collect();
@@ -28,11 +31,14 @@ fn part_one() -> usize {
             let count = map.entry(val).or_insert(0);
             *count += 1;
         }
-        nums_collector += map.iter()
+        let total_matches = map.iter()
             .filter(| (_key, &value) | value == 2)
-            .map(|(_key, &value)| value)
-            .fold(0, |acc, _num| if acc < 1 { acc + 1 } else { acc * 2 });
-
+            .map(|(_key, &value)| value);
+        let num_matches = total_matches.clone().collect::<Vec<_>>().len();
+        
+        let matches_sum = total_matches.fold(0, |acc, _num| if acc < 1 { acc + 1 } else { acc * 2 });
+        lines.insert(indx, (num_matches, matches_sum));
+        nums_collector += matches_sum
     }
     nums_collector
 }
