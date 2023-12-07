377e13eeb19c1563c52556c279416d8a8f9fc6ff
akalustat <akalustat.singh@gmail.com>
2023-12-06 17:11:52 -0800

    (Day 6) FINALLY GOT IT! The math had an off by one eror

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -10,7 +10,100 @@ struct Race {
     record: usize,
 }
 
-/*
+fn part_one() -> usize {
+    let lines: Vec<_> = get_files_lines("./aoc-inputs/2023/day6.txt");
+    let data_strs: Vec<_> = lines
+        .iter()
+        .map(|s| s.split(":").last().unwrap().trim())
+        .collect();
+
+    let times = data_strs[0].trim().split(" ").filter(|s| s != &"");
+    let mut distance_records = data_strs[1].trim().split(" ").filter(|s| s != &"");
+
+    let mut data: Vec<Race> = vec![];
+
+    for value in times.into_iter() {
+        data.push(Race {
+            total_time: value.parse::<usize>().unwrap(),
+            record: distance_records.next().unwrap().parse::<usize>().unwrap(),
+        });
+    }
+
+    let mut record_beating_collector = 1;
+    for race in data.iter() {
+        let range = get_range(race.total_time, race.record);
+        record_beating_collector *= range.count()
+    }
+
+    record_beating_collector
+}
+
+fn part_two() -> usize {
+    let lines: Vec<_> = get_files_lines("./aoc-inputs/2023/day6.txt");
+    let data_strs: Vec<_> = lines
+        .iter()
+        .map(|s| s.split(":").last().unwrap().trim())
+        .collect();
+
+    let time = data_strs[0]
+        .trim()
+        .split(" ")
+        .filter(|s| s != &"")
+        .collect::<Vec<_>>()
+        .join("");
+    let distance_record = data_strs[1]
+        .trim()
+        .split(" ")
+        .filter(|s| s != &"")
+        .collect::<Vec<_>>()
+        .join("");
+
+    let race = Race {
+        total_time: time.parse::<usize>().unwrap(),
+        record: distance_record.parse::<usize>().unwrap(),
+    };
+
+    get_range(race.total_time, race.record).count()
+}
+
+fn get_range(b: usize, c: usize) -> core::ops::RangeInclusive<usize> {
+     let b_sq = (b.pow(2) as f32).floor();
+    let second_part = 4.0 * c as f32;
+    let discriminant: f32 = (b_sq - second_part).sqrt();
+
+    let exact_root_1 = ((1.0 * b as f32) - discriminant) / 2.0;
+    let exact_root_2 = ((1.0 * b as f32) + discriminant ) / 2.0;
+
+    let mut root_1 = exact_root_1.ceil() as usize;
+    let mut root_2 = exact_root_2.floor() as usize;
+
+    
+    while test_value(&root_1, &b, &c) {
+       root_1 += 1; 
+    }
+    
+    while test_value(&root_2, &b, &c) {
+        root_2 -= 1;
+    }
+
+    (root_1)..=(root_2)
+}
+
+fn test_value(value: &usize, time: &usize, record: &usize) -> bool {
+    (((*time as f32) - *value as f32)  * *value as f32) - *record as f32 == 0.0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn correct_outputs() {
+        assert_eq!(625968, part_one());
+        assert_eq!(43663326, part_two());
+    }
+}
+
+/* Math
  * Given that the total distance traveled, d = (total_time, T, - hold_time, H) * H
  * We want the distances such that d > record, R
  *
@@ -56,102 +149,3 @@ struct Race {
  *
  *
  */
-
-fn part_one() -> f32 {
-    let lines: Vec<_> = get_files_lines("./aoc-inputs/2023/day6sample.txt");
-    let data_strs: Vec<_> = lines
-        .iter()
-        .map(|s| s.split(":").last().unwrap().trim())
-        .collect();
-
-    let times = data_strs[0].trim().split(" ").filter(|s| s != &"");
-    let mut distance_records = data_strs[1].trim().split(" ").filter(|s| s != &"");
-
-    let mut data: Vec<Race> = vec![];
-
-    for value in times.into_iter() {
-        data.push(Race {
-            total_time: value.parse::<usize>().unwrap(),
-            record: distance_records.next().unwrap().parse::<usize>().unwrap(),
-        });
-    }
-
-    let mut record_beating_collector: f32 = 1.0;
-    for race in data.iter() {
-        // println!("{}, {}", get_range(race.total_time, race.record), record_beating_collector);
-        let range = get_range(race.total_time, race.record);
-        record_beating_collector *= (range.end - range.start);
-    }
-
-    record_beating_collector
-}
-
-fn part_two() -> usize {
-    let lines: Vec<_> = get_files_lines("./aoc-inputs/2023/day6.txt");
-    let data_strs: Vec<_> = lines
-        .iter()
-        .map(|s| s.split(":").last().unwrap().trim())
-        .collect();
-
-    let time = data_strs[0]
-        .trim()
-        .split(" ")
-        .filter(|s| s != &"")
-        .collect::<Vec<_>>()
-        .join("");
-    let distance_record = data_strs[1]
-        .trim()
-        .split(" ")
-        .filter(|s| s != &"")
-        .collect::<Vec<_>>()
-        .join("");
-    println!("time: {}, record: {}", time, distance_record);
-
-    let race = Race {
-        total_time: time.parse::<usize>().unwrap(),
-        record: distance_record.parse::<usize>().unwrap(),
-    };
-
-    let mut collection = 0;
-    for i in 0..race.total_time {
-        let dist = (race.total_time - i) * i;
-        if dist > race.record {
-            collection += 1;
-        }
-    }
-    collection
-    /*
-
-     let range = get_range(race.total_time, race.record);
-     range.end - range.start */
-}
-
-fn get_range(b: usize, c: usize) -> core::ops::Range<f32> {
-     let b_sq = (b.pow(2) as f32).floor();
-    let second_part = 4.0 * c as f32;
-    let discriminant: f32 = (b_sq - second_part).sqrt();
-    let mut root_1 = ((1.0 * b as f32) - discriminant) / 2.0;
-    let mut root_2 = ((1.0 * b as f32) + discriminant ) / 2.0;
-
-
-    root_1 = root_2.ceil();
-    root_2 = root_2.floor();
-
-    
-    // while test_value(&root_1, &b, &c) {
-    //     println!("{}", test_value(&root_1, &b, &c));
-    //    root_1 += 1.0; 
-    // }
-    
-    // while test_value(&root_2, &b, &c) {
-    //     println!("{}", test_value(&root_1, &b, &c));
-    //     root_2 -= 1.0;
-    // }
-
-    println!("{} {}", root_1, root_2);
-    (root_1)..(root_2)
-}
-
-fn test_value(value: &f32, time: &usize, record: &usize) -> bool {
-    (((*time as f32) - value) * value) - *record as f32 == 0.0
-}
