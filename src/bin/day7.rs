9a78f061057996ff8ec279438bca157af1d21c2b
akalustat <akalustat.singh@gmail.com>
2023-12-07 13:11:35 -0800

    (Day 7) Finish Part 1, setup Part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,20 +1,22 @@
 use adventofcode_2023::get_files_lines;
-use std::collections::HashMap;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::str::FromStr;
 
 // so we can iterate over enum values (just makes things easier for me)
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 fn main() {
-    part_one();
+    println!("Part One: {}", part_one("./aoc-inputs/2023/day7.txt"));
+    println!("Part Two: {}", part_two("./aoc-inputs/2023/day7sample.txt"));
 }
 
+// Part One
 #[derive(Debug, PartialEq, Eq, EnumIter, Hash, Copy, Clone, PartialOrd, Ord)]
 enum Card {
     Two = 1,
     Three = 2,
-    Four = 3, 
+    Four = 3,
     Five = 4,
     Six = 5,
     Seven = 6,
@@ -116,7 +118,7 @@ impl From<[Card; 5]> for Hand {
             Hand::TwoPair { value: card_arr }
         // Case: 1 pair - 2 same, 3 diff
         } else if twos == &1 && ones == &3 {
-            Hand::OnePair  { value: card_arr }
+            Hand::OnePair { value: card_arr }
         }
         // case: High card - all diff
         else {
@@ -129,47 +131,25 @@ impl From<[Card; 5]> for Hand {
 impl Hand {
     fn as_value(&self) -> &[Card; 5] {
         match self {
-            Hand::FiveKind {value} => value,
-            Hand::FourKind {value} => value,
-            Hand::FullHouse {value} => value,
-            Hand::ThreeKind {value} => value,
-            Hand::TwoPair {value} => value,
-            Hand::OnePair {value} => value,
-            Hand::HighCard {value} => value,
+            Hand::FiveKind { value } => value,
+            Hand::FourKind { value } => value,
+            Hand::FullHouse { value } => value,
+            Hand::ThreeKind { value } => value,
+            Hand::TwoPair { value } => value,
+            Hand::OnePair { value } => value,
+            Hand::HighCard { value } => value,
         }
     }
 }
-// using https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html
-/*impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self < other {
-           return Some(Ordering::Greater);
-        } else if other < self {
-            return Some(Ordering::Less);
-        }  
 
-        // if let
-        for (i, card) in self.as_value().iter().enumerate() {
-            if card > &other.as_value()[i] {
-                return Some(Ordering::Greater);
-            } else if &other.as_value()[i] > card {
-                return Some(Ordering::Less);
-            }
-
-        }
-        return Some(Ordering::Equal);
-    }
-
+#[derive(Debug)]
+struct Play {
+    hand: Hand,
+    bet: usize,
 }
-// use partial ord to impl ord as noted here https://www.reddit.com/r/rust/comments/11gm19h/why_eq_partialeq_ord_and_partialord_especially/
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-       self.partial_cmp(other).unwrap()
-    }
-} */
 
-fn part_one() -> usize {
-    let lines: Vec<_> = get_files_lines("./aoc-inputs/2023/day7sample.txt");
+fn part_one(file_path: &str) -> usize {
+    let lines: Vec<_> = get_files_lines(file_path);
 
     let map: Vec<([Card; 5], usize)> = lines
         .iter()
@@ -191,16 +171,20 @@ fn part_one() -> usize {
             }
         })
         .collect::<Vec<_>>();
-    println!("{:?}", map);
 
-    let mut hands: Vec<(Hand, usize)> = map
+    let mut hands: Vec<Play> = map
         .iter()
         .map(|(cards, bet)| {
             let hand = Hand::from(*cards);
-            (hand, *bet)
+            Play { hand, bet: *bet }
         })
         .collect::<Vec<_>>();
-    hands.sort_by(|(hand1, _), (hand2, _)| {
+
+    // sorts in reverse order; we can then use the index to get the rank
+    hands.sort_by(|play1, play2| {
+        let Play { hand: hand1, .. } = play1;
+        let Play { hand: hand2, .. } = play2;
+
         if hand1 > hand2 {
             return Ordering::Greater;
         } else if hand2 > hand1 {
@@ -208,16 +192,247 @@ fn part_one() -> usize {
         } else {
             let hand1_vals = &hand1.as_value();
             let hand2_vals = &hand2.as_value();
-            for (i, card) in &hand1_vals.iter().enurmerate() {
-                if card > hand2_vals[i] {
+            println!("hand 1: {:?}, hand 2: {:?}", hand1_vals, hand2_vals);
+            for (i, card) in hand1_vals.iter().enumerate() {
+                if *card > hand2_vals[i] {
                     return Ordering::Greater;
-                } else if hand2_vals[i] > card {
+                } else if hand2_vals[i] > *card {
                     return Ordering::Less;
                 }
             }
             return Ordering::Equal;
         }
     });
-    println!("{:?}", hands);
-    0
+    hands
+        .iter()
+        .enumerate()
+        .map(|(indx, Play { bet, .. })| bet * (indx + 1))
+        .fold(0, |acc, val| acc + val)
+}
+
+// Part Two
+#[derive(Debug, PartialEq, Eq, EnumIter, Hash, Copy, Clone, PartialOrd, Ord)]
+enum CardTwo {
+    J = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    T = 10,
+    Q = 11,
+    K = 12,
+    A = 13,
+}
+
+// from https://www.reddit.com/r/rust/comments/2vqama/comment/cojzafn/?utm_source=share&utm_medium=web2x&context=3
+impl FromStr for CardTwo {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<CardTwo, ()> {
+        match s {
+            "A" => Ok(CardTwo::A),
+            "K" => Ok(CardTwo::K),
+            "Q" => Ok(CardTwo::Q),
+            "J" => Ok(CardTwo::J),
+            "T" => Ok(CardTwo::T),
+            "9" => Ok(CardTwo::Nine),
+            "8" => Ok(CardTwo::Eight),
+            "7" => Ok(CardTwo::Seven),
+            "6" => Ok(CardTwo::Six),
+            "5" => Ok(CardTwo::Five),
+            "4" => Ok(CardTwo::Four),
+            "3" => Ok(CardTwo::Three),
+            "2" => Ok(CardTwo::Two),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[repr(u8)]
+enum HandTwo {
+    HighCard { value: [CardTwo; 5] },
+    OnePair { value: [CardTwo; 5] },
+    TwoPair { value: [CardTwo; 5] },
+    ThreeKind { value: [CardTwo; 5] },
+    FullHouse { value: [CardTwo; 5] },
+    FourKind { value: [CardTwo; 5] },
+    FiveKind { value: [CardTwo; 5] },
+}
+
+impl From<[CardTwo; 5]> for HandTwo {
+    fn from(card_arr: [CardTwo; 5]) -> Self {
+        let mut times = HashMap::new();
+        for card in CardTwo::iter() {
+            let num_card = card_arr.into_iter().filter(|&val| val == card).count();
+            if num_card >= 1 {
+                times
+                    .entry(card)
+                    .and_modify(|count| *count += &1)
+                    .or_insert(num_card);
+            }
+        }
+
+        println!("{:?}", times); 
+        let number_of_cards: Vec<_> = times.into_iter().collect();
+
+        let num_js = &number_of_cards
+            .iter()
+            .filter(|(key, _)| key == &CardTwo::J)
+            .count();
+
+        let mut fives = &mut number_of_cards
+            .iter()
+            .filter(|(_, value)| value == &5)
+            .count();
+        let mut fours = &mut number_of_cards
+            .iter()
+            .filter(|(_, value)| value == &4)
+            .count();
+        let mut threes = &mut number_of_cards
+            .iter()
+            .filter(|(_, value)| value == &3)
+            .count();
+        let mut twos = &mut number_of_cards
+            .iter()
+            .filter(|(_, value)| value == &2)
+            .count();
+        let mut ones = &mut number_of_cards
+            .iter()
+            .filter(|(_, value)| value == &1)
+            .count();
+
+        if num_js > &0 {
+            if fours == &1 {
+                *fours = 0;
+                *fives += 1;
+            } else if threes == &1 {
+                *threes = 0;
+                *fours += 1;
+            }
+        }
+
+        // Case: Five of a Kind - All same
+        if fives == &1 {
+            HandTwo::FiveKind { value: card_arr }
+        // Case: Four of a kind - 4 same, 1 diff
+        } else if fours == &1 {
+            HandTwo::FourKind { value: card_arr }
+        // Case: Full House - 3 same, 2 same
+        } else if threes == &1 && twos == &1 {
+            HandTwo::FullHouse { value: card_arr }
+        // Case: Three of a kind - 3 same, 2 diff
+        } else if threes == &1 && ones == &2 {
+            HandTwo::ThreeKind { value: card_arr }
+        // Case: Two pair - 2 same, 2 same, 1 diff
+        } else if twos == &2 && ones == &1 {
+            HandTwo::TwoPair { value: card_arr }
+        // Case: 1 pair - 2 same, 3 diff
+        } else if twos == &1 && ones == &3 {
+            HandTwo::OnePair { value: card_arr }
+        }
+        // case: High card - all diff
+        else {
+            HandTwo::HighCard { value: card_arr }
+        }
+    }
+}
+
+// using https://users.rust-lang.org/t/accessing-enum-variant-trait-method-without-matching/59505/2
+impl HandTwo {
+    fn as_value(&self) -> &[CardTwo; 5] {
+        match self {
+            HandTwo::FiveKind { value } => value,
+            HandTwo::FourKind { value } => value,
+            HandTwo::FullHouse { value } => value,
+            HandTwo::ThreeKind { value } => value,
+            HandTwo::TwoPair { value } => value,
+            HandTwo::OnePair { value } => value,
+            HandTwo::HighCard { value } => value,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct PlayTwo {
+    hand: HandTwo,
+    bet: usize,
+}
+
+fn part_two(file_path: &str) -> usize {
+    let lines: Vec<_> = get_files_lines(file_path);
+
+    let map: Vec<([CardTwo; 5], usize)> = lines
+        .iter()
+        .map(|s| {
+            if let Some((cards, bet)) = s.split_once(" ") {
+                let mut cards_arr: [CardTwo; 5] = [CardTwo::Two; 5];
+                let parsed_cards = cards
+                    .split("")
+                    .filter(|s| s != &"")
+                    .map(|card| card.parse::<CardTwo>().unwrap());
+
+                for (i, card) in parsed_cards.enumerate() {
+                    cards_arr[i] = card;
+                }
+
+                (cards_arr, bet.parse::<usize>().unwrap())
+            } else {
+                panic!("Could not parse line");
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let mut hands: Vec<PlayTwo> = map
+        .iter()
+        .map(|(cards, bet)| {
+            let hand = HandTwo::from(*cards);
+            PlayTwo { hand, bet: *bet }
+        })
+        .collect::<Vec<_>>();
+
+    // sorts in reverse order; we can then use the index to get the rank
+    hands.sort_by(|play1, play2| {
+        let PlayTwo { hand: hand1, .. } = play1;
+        let PlayTwo { hand: hand2, .. } = play2;
+
+        if hand1 > hand2 {
+            return Ordering::Greater;
+        } else if hand2 > hand1 {
+            return Ordering::Less;
+        } else {
+            let hand1_vals = &hand1.as_value();
+            let hand2_vals = &hand2.as_value();
+            println!("hand 1: {:?}, hand 2: {:?}", hand1_vals, hand2_vals);
+            for (i, card) in hand1_vals.iter().enumerate() {
+                if *card > hand2_vals[i] {
+                    return Ordering::Greater;
+                } else if hand2_vals[i] > *card {
+                    return Ordering::Less;
+                }
+            }
+            return Ordering::Equal;
+        }
+    });
+    hands
+        .iter()
+        .enumerate()
+        .map(|(indx, PlayTwo { bet, .. })| bet * (indx + 1))
+        .fold(0, |acc, val| acc + val)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn part_one_is_correct() {
+        assert_eq!(6440, part_one("./aoc-inputs/2023/day7sample.txt"));
+        assert_eq!(252295678, part_one("./aoc-inputs/2023/day7.txt"));
+    }
+
+    fn part_two_is_correct() {}
 }
