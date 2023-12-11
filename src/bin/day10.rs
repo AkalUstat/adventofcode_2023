3a66e71884d0c37e851ccc684bd597762a09956c
akalustat <akalustat.singh@gmail.com>
2023-12-10 18:04:28 -0800

    (Day 10) small iterations on the concept

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -5,16 +5,13 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 
-struct Coordinate {
-    x: usize,
-    y: usize
-}
 #[derive(Debug, PartialEq, Eq)]
 enum Direction {
     // I for invalid and T for start
     N, E, S, W, I, T
 }
 
+#[derive(Debug)]
 enum Pipe {
     Vertical,
     Horizontal,
@@ -24,6 +21,13 @@ enum Pipe {
     SWBend,
     Ground,
     Start
+}
+
+#[derive(Debug)]
+struct LoopElement {
+    pipe: Pipe,
+    x: usize,
+    y: usize
 }
 
 impl FromStr for Pipe {
@@ -64,7 +68,40 @@ impl Pipe {
     }
 }
 
-fn main(){}
+fn main(){
+    part_one("./aoc-inputs/2023/day10sample.txt");
+}
 
-fn part_one() {}
+fn part_one(file_path: &str) {
+    let lines = get_files_lines(file_path);
+    let characters: Vec<Vec<_>> = lines.iter().map(|line| line.chars().collect::<Vec<_>>()).collect();
+
+    let mut start =  LoopElement {
+        pipe: Pipe::Start,
+        x: 0,
+        y: 0
+    };
+
+    'outer: for (y, line) in characters.iter().enumerate() {
+        for (x, char) in line.iter().enumerate() {
+            if char == &'S' {
+                start = LoopElement {
+                    x,
+                    y,
+                    ..start
+                };
+                break 'outer;
+            }
+        }
+    }
+
+    
+    println!("{:#?}", start);
+}
+
+fn find_loop() {
+
+}
+
+
 fn part_two() {}
